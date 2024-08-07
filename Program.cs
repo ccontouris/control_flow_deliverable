b148e76f88e85c6d6640ca8709ba453f6082eca1
ccontouris <ccontouris@icstars.org>
2024-08-06 20:30:38 -0500

    deliverable first entry

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int score = 75;
+char grade;
+
+if (score >= 90)
+{
+    grade = 'A';
+}
+else if (score >= 80)
+{
+    grade = 'B';
+}
+else if (score >= 70)
+{
+    grade = 'C';
+}
+else if (score >= 60)
+{
+    grade = 'D';
+}
+else
+{
+
+grade = 'F';
+
+}
+Console.WriteLine("Your grade is " + grade);
