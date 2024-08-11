7e675b77de119808304fd1a64bfdcdf79923b060
ccontouris <ccontouris@icstars.org>
2024-08-10 20:10:51 -0500

    woah done cool

--- Program.cs
+++ Program.cs
@@ -1,26 +1,89 @@
-﻿int score = 75;
-char grade;
+// namespace control_flow_deliverable
+// {
+//    public class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int score = 75;
+//             char grade;
 
-if (score >= 90)
-{
-    grade = 'A';
-}
-else if (score >= 80)
-{
-    grade = 'B';
-}
-else if (score >= 70)
-{
-    grade = 'C';
-}
-else if (score >= 60)
-{
-    grade = 'D';
-}
-else
-{
+//             if (score >= 90)
+//             {
+//                 grade = 'A';
+//             }
+//             else if (score >= 80)
+//             {
+//                 grade = 'B';
+//             }
+//             else if (score >= 70)
+//             {
+//                 grade = 'C';
+//             }
+//             else if (score >= 60)
+//             {
+//                 grade = 'D';
+//             }
+//             else
+//             {
 
-grade = 'F';
+//                 grade = 'F';
 
+//             }
+//             System.Console.WriteLine("Your grade is: " + grade);
+//         }
+
+//     }
+// }
+
+// using System.Data.Common;
+
+// namespace control_flow_deliverable
+// {
+//  public class SwitchCase
+//     {
+//         static void Main(string[] args)
+//         {
+//             int score = 77;
+//             switch (score)
+//             {
+//                 case  >= 90 :
+//                     Console.WriteLine("Grade A");
+//                     break;
+
+//                 case  >= 80 :
+//                     Console.WriteLine("Grade B");
+//                     break;
+
+//                 case >= 70 :
+//                     Console.WriteLine("Grade C");
+//                     break;
+
+//                 case >= 60 :
+//                     Console.WriteLine("Grade D");
+//                     break;
+
+//                 default:
+//                     Console.WriteLine("FAILLLL");
+//                     break;
+
+//             }
+//         }
+//     }
+// }
+
+namespace control_flow_deliverable
+{
+    class Ternary
+    {
+        static void Main(string[] args)
+        {
+            int score = 88;
+            char grade = score >= 90 ? 'A' :
+                         score >= 80 ? 'B' :
+                         score >= 70 ? 'C' :
+                         score >= 60 ? 'D' : 'F';
+
+                    System.Console.WriteLine(grade);
+        }
+    }
 }
-Console.WriteLine("Your grade is " + grade);
--- obj/Debug/net8.0/control_flow_deliverable.AssemblyInfo.cs
+++ obj/Debug/net8.0/control_flow_deliverable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("control_flow_deliverable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b148e76f88e85c6d6640ca8709ba453f6082eca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("control_flow_deliverable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("control_flow_deliverable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
